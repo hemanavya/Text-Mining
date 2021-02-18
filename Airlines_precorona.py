9d62afab2399d63aed207b7fa13419992f43ea51
Hema B <65726369+hemanavya@users.noreply.github.com>
2021-02-18 17:30:02 +0530

    Add files via upload

---
+++ Airlines_precorona.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### Import Libraries required
+
+import pandas as pd
+import numpy as np
+import string
+import re
+from pprint import pprint
+
+# NLTK 
+import nltk
+#nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')#language specific
+
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+# spacy for lemmatization
+import sys
+import spacy
+
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim  
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# Enable logging for gensim 
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
+
+# In[11]:
+
+
+df=pd.read_csv(r'C:\Users\rupesh.bobbili\Documents\Capstone\Airlines_Pre_full.csv',sep=',',encoding = "ISO-8859-1")  ### Read CSV file
+
+
+# In[12]:
+
+
+df.shape[0] 
+
+
+# In[13]:
+
+
+df.head(1)   ### 1st row
+
+
+# In[16]:
+
+
+data=df['text'].values.tolist()
+pprint(data[:1])  ### text/tweet column
+
+
+# In[17]:
+
+
+####Tokenize words and text Clean-up 
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  ### deacc=True -->removes punctuations
+
+data_words = list(sent_to_words(data))
+
+print(data_words[:1])
+
+
+# In[18]:
+
+
+### Building the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) 
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+
+### Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+
+print(trigram_mod[bigram_mod[data_words[0]]])
+
+
+# In[19]:
+
+
+#### Extra Stop words to be removed from corpus 
+stop_words = stopwords.words('english')
+sw_list = ['airline','airlines','AirTravel','flights','airports','airtravel','aviation','also','https','http','industry','com','airlineindustry','www',
+           'airplane','amp','make','bit','get']
+stop_words.extend(sw_list)
+
+
+# In[20]:
+
+
+### Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+# In[21]:
+
+
+### Calling the fucntions in order
+
+## Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+
+## Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+
+## Initialize spacy 'en_core_web_sm' model, keeping only tagger component (for efficiency)
+import spacy
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
+## Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+print(data_lemmatized[:1])
+
+
+# In[22]:
+
+
+### Creating dictinary and Corpus needed for topic modelling
+
+id2word = corpora.Dictionary(data_lemmatized)
+
+## Create Corpus
+texts = data_lemmatized
+
+## Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+
+## View
+print(corpus[:1])
+
+
+# In[23]:
+
+
+id2word[0]   ## to check what word a given ID corresponds to
+
+
+# In[24]:
+
+
+# Human readable format of corpus (term-frequency)
+[[(id2word[id], freq) for id, freq in cp] for cp in corpus[:2]]
+
+
+# In[25]:
+
+
+
+### Building LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=10, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=4,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+
+# In[26]:
+
+
+### Print the Keywords in the 10 topics
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]
+
+
+# In[27]:
+
+
+
+# Compute Perplexity
+print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # measure of how good the model is, lower the better.
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+
+# In[28]:
+
+
+# Visualize the topics
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+vis
+
+
+# In[29]:
+
+
+#### Finding the dominant topic in each sentence
+
+def format_topics_sentences(ldamodel=None, corpus=corpus, texts=data):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+       # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data_lemmatized)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+
+# Show
+df_dominant_topic.head(10)
+
+
+# In[30]:
+
+
+####  Wordcloud of Top N words in each topic
+
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=stop_words,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(3, 3, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
